8f0986d6b99484c7d6c949587eac7ad27f122374
git/ <jailsonsilvaoto@gmail.com>
2023-03-23 12:11:18 -0300

    header done

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,6 +1,25 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
+    
 `;
 
+export const Header = styled.div`
+    background-color: gray;
+    height: 150px;
+    text-align: center;
+`;
+
+export const HeaderText = styled.h1`
+    margin: 0;
+    padding: 0;
+    color: #b10505;
+    padding-top: 30px;
+`;
+
+export const  Body = styled.div`
+    margin: auto;
+    max-width: 980px;
+    background-color: #d3d3d3;
+    margin-bottom: 50px;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import * as C from "./App.styles"
 const App = () => {
   return (
     <C.Container>
-      ....
+      <C.Header>
+        <C.HeaderText>Financial System</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        ...Conteudo
+      </C.Body>
     </C.Container>
   );
 }
