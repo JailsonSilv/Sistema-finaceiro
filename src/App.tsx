719b6e5e6c79e89a2e8c5501668f052bc1a79b78
git/ <jailsonsilvaoto@gmail.com>
2023-03-24 13:32:39 -0300

    InfoArea Ok filter new month

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { items } from './data/items'
 import { categories } from './data/categories'
 import { getCurrentMonth, filterListByMonth } from "./helpers/dateFilter";
 import { TableArea } from "./components/TableArea"
+import { InfoArea } from "./components/InfoArea"
 
 const App = () => {
   const [list, setList] = useState(items);
@@ -16,6 +17,10 @@ const App = () => {
     setFilteredList(filterListByMonth(list, currentMonth))
   }, [list, currentMonth])
 
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  }
+
   return (
     <C.Container>
       <C.Header>
@@ -23,14 +28,17 @@ const App = () => {
       </C.Header>
       <C.Body>
         {/* Area de informações */}
-
+        <InfoArea 
+          currentMonth={currentMonth} 
+          onMonthChange={handleMonthChange}
+        />
         
         {/* Area de inserção */}
 
 
 
         {/* Tabela de itens */}
-        {<TableArea list={filteredList} />}
+        <TableArea list={filteredList} />
 
       </C.Body>
     </C.Container>
